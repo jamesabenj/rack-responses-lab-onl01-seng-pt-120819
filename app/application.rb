3c8a7149bd023f79906d4eea828592e5dd53a792
James Benjamin <james.abenj@gmail.com>
2020-03-06 20:51:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time::now.to_i < 12
+    time = Time.new
+    if time.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
